574a9121b62d4fc4d9fb78add2755963f9503866
tsolmon khorloo <tsolmonkhorloo@tsolmons-MacBook-Pro.local>
2023-10-06 18:42:43 +0800

    feat: ckeditor

--- src/components/form/ckeditor.tsx
+++ src/components/form/ckeditor.tsx
@@ -1,6 +1,8 @@
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
+// import { SimpleUploadAdapter } from '@ckeditor/ckeditor5-upload';
 import React, { useEffect, useRef } from 'react';
+// import SimpleuploadPlugin from 'ckeditor5-simple-upload/src/simpleupload';
 
 interface CKeditorProps {
   onChange: (data: string) => void;
@@ -21,11 +23,32 @@ export default function CKeditor({
   }>();
   useEffect(() => {
     editorRef.current = {
-      CKEditor: CKEditor,
-      ClassicEditor: ClassicEditor,
+      CKEditor: require('@ckeditor/ckeditor5-react').CKEditor,
+      ClassicEditor: require('@ckeditor/ckeditor5-build-classic'),
     };
   }, []);
-
+  // ClassicEditor.create(document.querySelector('#editor'), {
+  //   plugins: [SimpleUploadAdapter /* ... */],
+  //   toolbar: [
+  //     /* ... */
+  //   ],
+  //   simpleUpload: {
+  //     // The URL that the images are uploaded to.
+  //     uploadUrl: 'http://example.com',
+  //
+  //     // Enable the XMLHttpRequest.withCredentials property.
+  //     withCredentials: true,
+  //
+  //     // Headers sent along with the XMLHttpRequest to the upload server.
+  //     headers: {
+  //       'X-CSRF-TOKEN': 'CSRF-Token',
+  //       Authorization: 'Bearer <JSON Web Token>',
+  //     },
+  //   },
+  // })
+  //   .then(/* ... */)
+  //   .catch(/* ..*/);
+  //
   return (
     <>
       {editorLoaded ? (
@@ -39,6 +62,8 @@ export default function CKeditor({
           config={{
             toolbar: [
               'heading',
+              'imageUpload',
+              'link',
               '|',
               'bold',
               'italic',
